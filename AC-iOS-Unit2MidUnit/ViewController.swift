dbc89335a9b9bc026570e6bcd56672e0355c8272
Melissa He <melissahe@ac.c4q.nyc>
2017-10-27 17:13:33 -0400

    Finish unit 2 midunit assessment

--- AC-iOS-Unit2MidUnit/ViewController.swift
+++ AC-iOS-Unit2MidUnit/ViewController.swift
@@ -8,11 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var poloniusMonologueTextView: UITextView!
+    @IBOutlet weak var romeoJulietTextField: UITextField!
+    @IBOutlet weak var romeoJulietTextView: UITextView!
+    
+    var polonius = PoloniusMonologueModel()
+    var romeoJuliet = RomeoAndJulietModel()
     
     override func viewDidLoad() {
+        roundEdgesOf(poloniusMonologueTextView)
+        roundEdgesOf(romeoJulietTextView)
+        poloniusMonologueTextView.text = polonius.firstLine
+        romeoJulietTextView.text = ""
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        romeoJulietTextField.delegate = self
+    }
+    
+    @IBAction func nextLineButtonPressed(_ sender: UIButton) {
+        poloniusMonologueTextView.text = polonius.nextLine()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let inputName = textField.text else {
+            return false
+        }
+        romeoJulietTextView.text = romeoJuliet.name(of: inputName).lines
+        switch romeoJuliet.name(of: inputName).exists {
+        case true:
+            textField.resignFirstResponder()
+            return true
+        case false:
+            return false
+        }
+    }
+    
+    func roundEdgesOf(_ textView: UITextView) {
+        textView.layer.cornerRadius = 10
+        textView.layer.masksToBounds = true
+        textView.layer.borderWidth = 1
+        textView.layer.borderColor = UIColor.init(red: 0.745, green: 0.278, blue: 0.309, alpha: 1).cgColor
     }
     
 }
