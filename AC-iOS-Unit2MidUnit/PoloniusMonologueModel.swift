06522627be207f07b9d1ec50654bf2e02a8ea272
Ashlee Krammer <ashlee.alyse@me.com>
2017-10-27 18:03:02 -0400

    Almost Done

--- AC-iOS-Unit2MidUnit/PoloniusMonologueModel.swift
+++ AC-iOS-Unit2MidUnit/PoloniusMonologueModel.swift
@@ -8,27 +8,35 @@
 
 import Foundation
 
-//var appendArr
 
 class PoloniusMonologueModel {
+    var currentLine = ""
+    var strIndex = 0
+    //var counter = 0
+    
     //Complete this model
-    private let textArr = ["My liege, and madam, to expostulate",
-                 "What majesty should be, what duty is,",
-                 "What day is day, night night, and time is time,",
-                 "Were nothing but to waste night, day, and time;",
-                 "Therefore, since brevity is the soul of wit,",
-                 "And tediousness the limbs and outward flourishes,",
-                 "I will be brief. Your noble son is mad."
-                ]
+    //Monologue Arr of Str
+     let textArr = ["My liege, and madam, to expostulate",
+                           "What majesty should be, what duty is,",
+                           "What day is day, night night, and time is time,",
+                           "Were nothing but to waste night, day, and time;",
+                           "Therefore, since brevity is the soul of wit,",
+                           "And tediousness the limbs and outward flourishes,",
+                           "I will be brief. Your noble son is mad."
+    ]
     
-    func addLine() -> Bool {
-        for line in textArr {
-            print(line)
+    func nextLine() {
+        for lineIndex in 0..<textArr.endIndex {
+            for line in textArr {
+                strIndex = lineIndex
+                //print("str index from model: \(strIndex), current line from model \(currentLine)")
+                strIndex += 1
+                currentLine = line
+                //print(currentLine)
+             //counter += 1
+            }
         }
-        return true
     }
-    
-    
     
     
     
--- AC-iOS-Unit2MidUnit/ViewController.swift
+++ AC-iOS-Unit2MidUnit/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIScrollViewDelegate {
     
     var counter = 0
     var monoModel = PoloniusMonologueModel()
@@ -23,7 +23,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     //Scroll View Vars
     @IBOutlet weak var InputFromMono: UIScrollView! //P1
+    @IBOutlet weak var ponoMono: UILabel! //P1
     @IBOutlet weak var sceneScroll: UIScrollView! //P2
+    @IBOutlet weak var sceneTwo: UILabel! //P2
     
     //Next Line Button Outlet
     @IBOutlet weak var nextLineButton: UIButton! //P1
@@ -35,17 +37,54 @@ class ViewController: UIViewController, UITextFieldDelegate {
     //Override View Func 
     override func viewDidLoad() {
         super.viewDidLoad()
+        InputFromMono.delegate = self
+        sceneScroll.delegate = self
+        textBox.delegate = self
         // Do any additional setup after loading the view.
     }
     
-    //Action Button
-    @IBAction func nextLineAction(_ button: UIButton) {
-        counter += 1
-        monoModel.addLine()
+    //Action Button - Mid Unit Test Part 1
+    @IBAction func nextLineAction(_ sender: UIButton) {
+        monoModel.nextLine()
+        if counter < 7 {
+            ponoMono.text = monoModel.textArr[counter]
+            counter += 1
+        }
+        else {
+            ponoMono.text = monoModel.textArr[0]
+            return
+        }
         
+        //All the code I tried but didn't work
+        //let strIndex = monoModel.strIndex
+        
+        //        if strIndex == counter - 1{
+        //            let currentLine = monoModel.textArr[strIndex - 1]
+        //            ponoMono.text = currentLine
+        //            print("str Counter: \(strIndex), button counter: \(counter), current line: \(currentLine)")
+        
+    }
+    //currentLine = monoModel.textArr[0]
+    // counter = 0
+
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.text == "ROMEO" || textField.text == "MERCUTIO" || textField.text == "BENVOLIO" || {
+            if sceneModel.sceneFourTextArr.contains("ROMEO".uppercased()){
+                print("hey")
+            } else if  sceneModel.sceneFourTextArr.contains("MERCUTIO".uppercased()){
+                print("hey")
+            } else if 
     }
     
     
-    
+
 }
+
+
+
+
+}
+//}
+
 
