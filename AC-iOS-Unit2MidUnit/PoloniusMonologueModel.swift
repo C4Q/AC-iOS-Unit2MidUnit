624bdde0b776af5c673bae6b7906a7827b361108
Nezar Khabar <nezarkhabar@ac.c4q.nyc>
2017-10-31 13:34:22 -0400

    Done

--- AC-iOS-Unit2MidUnit/PoloniusMonologueModel.swift
+++ AC-iOS-Unit2MidUnit/PoloniusMonologueModel.swift
@@ -1,3 +1,4 @@
+
 //
 //  PoloniusMonologue.swift
 //  AC-iOS-Unit2MidUnit
@@ -9,13 +10,33 @@
 import Foundation
 
 class PoloniusMonologueModel {
+
+    
     //Complete this model
     private let textArr = ["My liege, and madam, to expostulate",
-                 "What majesty should be, what duty is,",
-                 "What day is day, night night, and time is time,",
-                 "Were nothing but to waste night, day, and time;",
-                 "Therefore, since brevity is the soul of wit,",
-                 "And tediousness the limbs and outward flourishes,",
-                 "I will be brief. Your noble son is mad."
-                ]
+                           "What majesty should be, what duty is,",
+                           "What day is day, night night, and time is time,",
+                           "Were nothing but to waste night, day, and time;",
+                           "Therefore, since brevity is the soul of wit,",
+                           "And tediousness the limbs and outward flourishes,",
+                           "I will be brief. Your noble son is mad."
+    ]
+    
+    //var delegate: PoloniusMonologueModelDelegate!
+    
+    private var messageString = ""
+    private var messageArrCounter = 0
+    func displayPoloniusMessage() -> String {
+        if messageArrCounter < textArr.count {
+            messageString = textArr[messageArrCounter]
+            messageArrCounter += 1
+            return messageString
+        } else {
+            messageArrCounter = 0
+             messageString = textArr[messageArrCounter]
+            messageArrCounter += 1
+            return messageString
+        }
+    }
+    
 }
