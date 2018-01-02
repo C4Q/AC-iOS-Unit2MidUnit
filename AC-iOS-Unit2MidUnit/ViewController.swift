b839d4faf807dd0d507553cfcbea9ed0c2a9cde8
Melissa He <melissahe@ac.c4q.nyc>
2018-01-02 15:32:20 -0500

    Add programmatic layout for polonius

--- AC-iOS-Unit2MidUnit/ViewController.swift
+++ AC-iOS-Unit2MidUnit/ViewController.swift
@@ -10,20 +10,79 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
+    var polonius = PoloniusMonologueModel()
+    var romeoJuliet = RomeoAndJulietModel()
+    
+    var poloniusStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.spacing = 15
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.axis = .vertical
+        
+        return stackView
+    }()
+    
+    var poloniusTitleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "Polonius Monologue"
+        label.textColor = UIColor(red: 0.925, green: 0.522, blue: 0.585, alpha: 1)
+        label.textAlignment = .center
+        label.font = UIFont(name: "Futura-Medium", size: 28)
+        
+        return label
+    }()
+    
+    lazy var poloniusTextView: UITextView = {
+        let textView = UITextView()
+        
+        textView.text = self.polonius.firstLine
+        textView.textColor = UIColor(red: 0.745, green: 0.278, blue: 0.309, alpha: 1)
+        textView.backgroundColor = .white
+        textView.font = UIFont(name: "Futura-MediumItalic", size: 18)
+        textView.textAlignment = .center
+        roundEdgesOf(textView)
+        
+        return textView
+    }()
+    
+    let nextLineButton: UIButton = {
+        let button = UIButton()
+        
+        button.setTitle("Next Line", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Futura-Medium", size: 20)
+        button.setTitleColor(UIColor(red: 0.745, green: 0.278, blue: 0.309, alpha: 1)
+, for: .normal)
+        button.titleLabel?.textAlignment = .center
+        
+        return button
+    }()
+    
     @IBOutlet weak var poloniusMonologueTextView: UITextView!
     @IBOutlet weak var romeoJulietTextField: UITextField!
     @IBOutlet weak var romeoJulietTextView: UITextView!
     
-    var polonius = PoloniusMonologueModel()
-    var romeoJuliet = RomeoAndJulietModel()
     
     override func viewDidLoad() {
-        roundEdgesOf(poloniusMonologueTextView)
-        roundEdgesOf(romeoJulietTextView)
-        poloniusMonologueTextView.text = polonius.firstLine
-        romeoJulietTextView.text = ""
         super.viewDidLoad()
-        romeoJulietTextField.delegate = self
+        
+        self.view.backgroundColor = UIColor(red: 1, green: 0.877, blue: 0.892, alpha: 1)
+        
+        poloniusStackView.addArrangedSubview(poloniusTitleLabel)
+        poloniusStackView.addArrangedSubview(poloniusTextView)
+        poloniusStackView.addArrangedSubview(nextLineButton)
+        
+        self.view.addSubview(poloniusStackView)
+        
+        setUpConstraints()
+        
+//        roundEdgesOf(poloniusMonologueTextView)
+//        roundEdgesOf(romeoJulietTextView)
+//        poloniusMonologueTextView.text = polonius.firstLine
+//        romeoJulietTextView.text = ""
+//        romeoJulietTextField.delegate = self
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -46,6 +105,26 @@ class ViewController: UIViewController, UITextFieldDelegate {
         case false:
             return false
         }
+    }
+    
+    func setUpConstraints() {
+       
+        poloniusStackView.translatesAutoresizingMaskIntoConstraints = false
+        poloniusStackView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        poloniusStackView.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+        poloniusStackView.heightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.3).isActive = true
+        poloniusStackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        
+        poloniusTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        poloniusTitleLabel.widthAnchor.constraint(equalTo: poloniusStackView.widthAnchor, multiplier: 0.8).isActive = true
+        poloniusTitleLabel.heightAnchor.constraint(equalTo: poloniusStackView.heightAnchor, multiplier: 0.25).isActive = true
+        poloniusTitleLabel.centerXAnchor.constraint(equalTo: poloniusStackView.centerXAnchor).isActive = true
+        
+        poloniusTextView.translatesAutoresizingMaskIntoConstraints = false
+        poloniusTextView.setContentHuggingPriority(UILayoutPriority(250), for: .horizontal)
+        poloniusTextView.setContentHuggingPriority(UILayoutPriority(250), for: .vertical)
+        poloniusTextView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        poloniusTextView.widthAnchor.constraint(equalTo: poloniusStackView.widthAnchor, multiplier: 0.6).isActive = true
     }
     
     func roundEdgesOf(_ textView: UITextView) {
